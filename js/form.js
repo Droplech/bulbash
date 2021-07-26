8b639c270a7b22e2bf35dacf374dbb02d1ffcb83
Maxim <53229586+Droplech@users.noreply.github.com>
2021-07-26 22:32:40 +0300

    commit

---
+++ js/form.js
@@ -0,0 +1,78 @@
+$(document).ready(function(){
+
+    $('#basket_form').bind("submit",checkForm);
+    function checkForm(e){
+        e.preventDefault()
+
+        var el = document.getElementById('basket_form')
+
+        var name = el.name.value;
+        var phone = el.phone.value;
+        var addres = el.addres.value;
+        var cell_back = el.cell_back.value
+        var fail = "";
+        var acept = "Форма заполнена";
+        
+        if(name == "" || phone == "" || addres == "" || cell_back == "")
+        fail = "Все поля должны быть заполнены корректно";
+
+        else if(name.length <= 1 || name.length > 50  )
+        fail = "Введите корректное имя";
+
+        else if(phone.length < 12 || phone.length > 12)
+        fail = "Номер введен некорректно"
+
+        
+        console.log("Name:" + " " + name + ";")
+        console.log("Phone"  + " " + phone + ";");
+        console.log("Addres"  + " " + addres + ";");
+        console.log("Cell back"  + " " + cell_back + ";");
+        if( fail != ""){
+            $("#error").html(fail)
+            $("#acept").html("")
+        }else{
+            $("#acept").html(acept)
+            $("#error").html("")
+            $('.modal_order_completed').fadeIn()
+            $('body,html').animate({scrollTop:0},300);
+            
+        }
+    }
+
+
+
+    $('#form_contacts').bind("submit",checkFormContacts);
+    function checkFormContacts(e){
+        e.preventDefault()
+
+        var el = document.getElementById('form_contacts')
+
+        var name = el.name.value;
+        var phone = el.phone.value;
+        var fail = "";
+        var acept = "Форма заполнена";
+        
+        if(name == "" || phone == "" )
+        fail = "Все поля должны быть заполнены корректно";
+
+        else if(name.length <= 1 || name.length > 50  )
+        fail = "Введите корректное имя";
+
+        else if(phone.length < 12 || phone.length > 12)
+        fail = "Номер введен некорректно"
+        
+        console.log("Name:" + " " + name + ";")
+        console.log("Phone"  + " " + phone + ";");
+        
+        if( fail != ""){
+            $("#error").html(fail)
+            $("#acept").html("")
+        }else{
+            $("#acept").html(acept)
+            $("#error").html("")
+            $('.modal_order_completed').fadeIn()
+            
+            
+        }
+    }
+})
--- js/script.js
+++ js/script.js
@@ -11,6 +11,67 @@ $(document).ready(function(){
 
   })
 
+  $('.add_in_basket .btn').click(function(e){
+    e.preventDefault()
+    $('.add_in_basket').addClass('add_in_basket-active')
+    
+    $(this).next('.add_message').fadeIn()
+    $(this)
+    
+  })
+
+
+  const counter = (selector, action) => {
+    let price = selector.parents('.basket_counter').find($('.count_info'));
+  
+    switch (action) {
+      case 'increment': 
+        price.val(parseInt(price.val()) + 1); break;
+      case 'decrement': 
+        if (price.val() < 1) { price.val(0) } else { price.val(parseInt(price.val()) - 1);} break;  
+    }
+  } 
+  
+  $('.count_plus').click(function() {
+  counter($(this), 'increment')
+  })
+
+  $('.count_minus').click(function() {
+  counter($(this), 'decrement')
+  })
+
+      
+  $('.basket_order_btn').click(function(e){
+    e.preventDefault()
+    $('.form_to_order').slideDown()
+    $('.basket_order_btn').css("display","none")
+    
+  })
+
+  // $('.basket_to_order_btn').click(function(){
+  //   $('.modal_order_completed').fadeIn()
+  // })
+
+  // $('.form_to_order form button').click(function(){
+
+  //   $('.modal_order_completed').fadeIn()
+
+
+  // })
+
+
+
+
+
+  $('.burger_button').click(function(){
+    if ( !$('.burger_button').hasClass('burger-active') ){
+      $('.burger_button').addClass('burger-active');
+      $('.burger_button').next('.burger_menu').slideDown()
+    }else{
+      $('.burger_button').removeClass('burger-active');
+      $('.burger_button').next('.burger_menu').slideUp()
+    }
+  })
 
 
 
@@ -20,32 +81,28 @@ $(document).ready(function(){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- new Swiper('.stock_slider_container',{
-    slidesPerView: 3,
+  new Swiper('.stock_slider_container',{
+    slidesPerView: 1,
     spaceBetween: 0,
+
     navigation: {
       nextEl: '#stock_next',
       prevEl: '#stock_prev',
     },
+
     pagination: {
       el: "#stock_pagination",
     },
- })   
+
+    breakpoints:{
+      700:{
+        slidesPerView: 2,
+      },
+      1280:{
+        slidesPerView: 3,
+      }
+    }
+  })   
 
 
 
